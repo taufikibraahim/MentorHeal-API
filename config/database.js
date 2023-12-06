acd9160e97cd4261a971f62586fb3171c87cc2dd
Irvan Na'syakban <irfanna.syakban098@gmail.com>
2023-12-06 09:54:38 +0700

    update database name

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('manajemen_spp', 'root', '', {
+const db = new Sequelize('mentorheal', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 })
